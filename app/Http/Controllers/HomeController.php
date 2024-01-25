116156df294cf5d7cebf1375bb25f495455b59a4
unknown <asde621@naver.cmom>
2024-01-25 15:13:14 +0900

    push

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(): string
+    {
+        return '¾È³çÇÏ¼¼¿ä from HomeController';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users/{name?}', function($name = null){
+    return '안녕,'. $name;
+})->where('name', '[a-zA-Z0-9]+');
+
+Route::get('/products/{id?}', function($id = null){
+    return '제품 id : '. $id;
+})->where('id', '[0-9]+');
